a6284eb3c49324c357b8a808e937e4c1b667e5cd
binghongli <b97607065@gmail.com>
2018-11-16 02:04:14 +0800

    comment and remove some useless folder

--- invoke_chaincode_lbh/Chaincode_Tutorial.go
+++ invoke_chaincode_lbh/Chaincode_Tutorial.go
@@ -8,12 +8,16 @@ import (
 
 type SampleChaincode struct {}
 
-//
+/*
+	必須將要調用的合約之相關參數轉成[][]byte，而後方能傳回
+	"被調用的合約名", "channel名","被調用合約的相關參數"
+*/
 // https://stackoverflow.com/questions/18559830/function-for-copying-arrays-in-go-language
 func (sc *SampleChaincode) invokeOtherChaincode(stub shim.ChaincodeStubInterface, args []string ) peer.Response{
 
 	chaincodeName := args[0]
 	channelName := args[1]
+	// 消除前兩個參數，剩下的都當作是被調用合約的相關參數
 	var byteArray [][]byte
 	for _, arg := range args[2:] {
 		byteArray = append(byteArray, []byte(arg))
